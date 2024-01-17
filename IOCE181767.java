f62dc2b6cc4fb9e6f7243e6bc27c9898793dd5df
Lê Nhựt Anh <117267049+lnanh2k4@users.noreply.github.com>
2024-01-18 05:12:13 +0700

    Update IOCE181767.java

--- IOCE181767.java
+++ IOCE181767.java
@@ -1,18 +1,13 @@
-package ce181767_lab06;
+package x;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 import java.util.Scanner;
 
 /**
  *
- * @author Le Nhut Anh - CE181767
+ * @author Le Nhut Anh
  */
-public class IOCE181767 {
+public class IOCEXXXXXX {
 
     public static Scanner sc = new Scanner(System.in);
 
