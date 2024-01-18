90f182f109d77989994a198caa0c5c81f42e835e
Lê Nhựt Anh <117267049+lnanh2k4@users.noreply.github.com>
2024-01-18 10:28:32 +0700

    Update IOHandleFile.java

--- IOHandleFile.java
+++ IOHandleFile.java
@@ -1,18 +1,11 @@
-package io;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 import java.util.Scanner;
 
 /**
  *
- * @author Le Nhut Anh - CE181767
+ * @author Le Nhut Anh
  */
-public class IOCE181767 {
+public class IOHandleFile {
 
     /**
      *
